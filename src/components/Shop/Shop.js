817ad917c1a07f535375b89b16928c48ef2ee54c
saimon1609 <20-44310-3@student.aiub.edu>
2021-10-02 16:08:52 +0600

    first commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import { addToDb } from '../../utilities/fakedb';
+import './Shop.css';
+import useCart from '../../hooks/useCart';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useCart(products);
+    // products to be rendered on the UI.
+    const [displayProducts, setDisplayProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data);
+                setDisplayProducts(data);
+            });
+    }, []);
+
+
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+        // save to local storage (for now)
+        addToDb(product.key);
+
+    }
+
+    const handleSearch = event => {
+        const searchText = event.target.value;
+
+        const matchedProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+
+        setDisplayProducts(matchedProducts);
+    }
+
+    return (
+        <>
+            <div className="search-container">
+                <input
+                    type="text"
+                    onChange={handleSearch}
+                    placeholder="Search Product" />
+            </div>
+            <div className="shop-container">
+                <div className="product-container">
+                    {
+                        displayProducts.map(product => <Product
+                            key={product.key}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        >
+                        </Product>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <Cart cart={cart}></Cart>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Shop;
